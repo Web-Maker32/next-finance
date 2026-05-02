5d0d094eaf2550a6eb15f3cddd83590462086193
webmaker <uroojnaumaan@gmail.com>
2026-05-02 15:12:26 +0500

    created form

--- app/dashboard/transactions/add/page.js
+++ app/dashboard/transactions/add/page.js
@@ -1,11 +1,13 @@
+import TransactionForm from "../../components/transaction-form";
 export const metadata = {
   title: "Add Transaction",
 };
 
-export default function page() {
-    return (
-        <div>
-            <h1 className="text-4xl font-semibold mb-8">Add Transaction</h1>
-        </div>
-    )
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold mb-8">Add Transaction</h1>
+      <TransactionForm />
+    </>
+  );
 }
---
+++ libs/consts.js
@@ -0,0 +1,6 @@
+export const types = [
+  'Income', 'Expense', 'Investment', 'Savings'
+]
+export const categories = [
+  'Housing', 'Transport', 'Health', 'Food', 'Education', 'Other'
+]
