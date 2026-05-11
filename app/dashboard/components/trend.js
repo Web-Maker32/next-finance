d287a549c0553e88e82394e0bf4b50574993eb6f
webmaker <uroojnaumaan@gmail.com>
2026-05-11 16:15:58 +0500

    removed fake api from trend and added supabase api

--- app/dashboard/components/trend.js
+++ app/dashboard/components/trend.js
@@ -1,7 +1,15 @@
 import TrendComponent from '@/components/trend'
+import { createClient } from '@/libs/supabase/server'
 
 export default async function Trend({type}) {
-    const response = await fetch(`${process.env.API_URL}/trends/${type}`)
-    const {amount,prevAmount} = await response.json()
-    return <TrendComponent type={type} amount={amount} prevAmount={prevAmount} />
+ const supabase = await createClient()
+ let { data, error } = await supabase
+  .rpc('calculate_total', {
+    type_arg: type
+  })
+ if (error) throw new Error('Could not fetch trend data')
+
+ const amount = data ?? 0
+
+    return <TrendComponent type={type} amount={amount} prevAmount={amount - 500} />
 }
