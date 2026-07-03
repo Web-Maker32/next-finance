b5dbfad916085555f7111b3dc53ac864f8a71282
webmaker <uroojnaumaan@gmail.com>
2026-07-03 17:56:20 +0500

    4

--- components/header.js
+++ components/header.js
@@ -31,7 +31,7 @@ export default async function Header({className}) {
          className={`${variants['ghost']} ${sizes['sm']} flex items-center`}
          aria-label="Sign in"
        >
-         <LogIn className="w-5 h-5 mr-2" />
+         <LogIn className="w-6 h-6 mr-2" />
          <span className="hidden sm:inline">Login</span>
        </Link>
      )}  
