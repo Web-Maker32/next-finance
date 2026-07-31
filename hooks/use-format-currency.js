ba468680612346a2158770b492bf378f940cd2a9
webmaker <uroojnaumaan@gmail.com>
2026-07-31 22:02:10 +0500

    fixed right currency symbol not showing

--- hooks/use-format-currency.js
+++ hooks/use-format-currency.js
@@ -1,9 +1,16 @@
 import { useMemo } from "react"
 
+const currencyLocaleMap = {
+  PKR: 'en-PK'
+}
+
 export const useFormatCurrency = (amount) =>{
     const currency = typeof window !== 'undefined' ? (localStorage.getItem('currency') || 'EUR') : 'EUR'
-    const locale = typeof window !== 'undefined' ? (navigator.language || 'en-US') : 'en-US'
+    const locale = typeof window !== 'undefined'
+      ? currencyLocaleMap[currency] || navigator.language || 'en-US'
+      : 'en-US'
     const formatCurrency = (amount) =>
-    new Intl.NumberFormat(locale, {style: 'currency', currency}).format(amount)
+      new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount)
 
-    return useMemo(() => formatCurrency(amount), [amount, currency, locale])} 
+    return useMemo(() => formatCurrency(amount), [amount, currency, locale])
+}
